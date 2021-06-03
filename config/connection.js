8de1d5ca2719dfbcef3e710c78561d38181986e3
Kevin Kunkel <Christopher.Kunkel@traviscountytx.gov>
2021-06-03 15:33:47 -0500

    file layout

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('just_tch_news_db','username','password',{
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.exports = sequelize;
